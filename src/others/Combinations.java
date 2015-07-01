d9af0baab696b7c6e76e1482a09c61f88fb30a2f
maxlpy <maxlipengyu@gmail.com>
2015-06-30 22:04:11 -0400

    permutations and combinations

---
+++ src/others/Combinations.java
@@ -0,0 +1,27 @@
+package others;
+
+public class Combinations {
+	public StringBuilder out = new StringBuilder();
+	public String in;
+	
+	public Combinations(String str) {
+		this.in = str;
+	}
+	public void combine() {
+		conbine(0);
+	}
+	public void conbine(int start) {
+		for (int i = start; i < in.length(); ++i) {
+			out.append(in.charAt(i));
+			System.out.println(out);
+			if (i < in.length()) 
+				conbine(i+1);
+			out.setLength(out.length()-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Combinations combination = new Combinations("abcd");
+		combination.combine();
+	}
+}
---
+++ src/others/Permutations.java
@@ -0,0 +1,34 @@
+package others;
+
+public class Permutations {
+	private boolean[] used;
+	private StringBuilder out = new StringBuilder();
+	private String in;
+	
+	public Permutations(String str) {
+		in = str;
+		used = new boolean[str.length()];
+	}
+	public void permute() {
+		if (out.length() == in.length()) {
+			System.out.println(out);
+			return;
+		}
+		for (int i = 0; i < in.length(); i++) {
+			if (used[i]) continue;
+			System.out.println(in.charAt(i));
+			out.append(in.charAt(i));
+			used[i] = true;
+			permute();
+			used[i] = false;
+			out.setLength(out.length()-1);
+			System.out.println(out);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Permutations test = new Permutations("abcd");
+		System.out.println("Start.");
+		test.permute();
+	}
+}
